9110492a32870ee54ad1a0a7b581903581b25e7a
dredd85 <piotr.siergiej@gmail.com>
2023-06-26 16:01:14 +0200

    Added file. Database creation

---
+++ 1.Create_DB.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('Stocks.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Stocks (
+id INTEGER PRIMARY KEY,
+symbol VARCHAR(10),
+company_name VARCHAR(50),
+industry VARCHAR(50)
+)''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Prices (
+id INTEGER PRIMARY KEY,
+stock_id INTEGER, 
+date TEXT,
+open_price REAL,
+high_price REAL,
+low_price REAL,
+close_price REAL,
+volume INTEGER,
+FOREIGN KEY (stock_id) REFERENCES stocks (id)
+)''')
+
+conn.commit()
+conn.close()
