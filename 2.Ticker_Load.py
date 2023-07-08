10a5f95e456f7a83760e50773f681b73f181e656
dredd85 <piotr.siergiej@gmail.com>
2023-07-08 13:44:16 +0200

    Working scrape of tickers

--- 2.Ticker_Load.py
+++ 2.Ticker_Load.py
@@ -1,19 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.gpw.pl/spolki"
+URL = "https://infostrefa.com/infostrefa/pl/spolki"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-tags = soup('span', class_='grey')
+tickers = []
 
-stock_symbols = list()
+tags = soup.find("table", class_="table table-text table-text-left custom-border")
 
-for tag in tags:
-    symbol = tag.text.strip()
-    symbol = symbol.replace("(", "").replace(")", "")  # Remove parentheses
-    stock_symbols.append(symbol)
+for tag in tags.find_all('tr'):
+    for row in tag:
+        strip_row = row.text.strip()
+        if len(strip_row) !=3:
+            continue
+        else:
+            tickers.append(strip_row)
 
-for stock in stock_symbols:
-    print(stock)
+print(*tickers)
