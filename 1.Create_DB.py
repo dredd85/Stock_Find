7dd854e2dfcc24475ea42f3849ea8ea2c70e9b15
Dredd85 <piotr.siergiej@gmail.com>
2023-07-11 19:45:45 +0200

    Added autoincrement in database creation

--- 1.Create_DB.py
+++ 1.Create_DB.py
@@ -2,17 +2,17 @@ import sqlite3
 
 conn = sqlite3.connect('Stocks.db')
 cursor = conn.cursor()
-
+ 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Stocks (
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 symbol VARCHAR(10),
 company_name VARCHAR(50),
 industry VARCHAR(50)
 )''')
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Prices (
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 stock_id INTEGER, 
 date DATE,
 open_price REAL,
