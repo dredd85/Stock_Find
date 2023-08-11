6e16bfd6c687db521d15f0bf8de8aa8ab8d757d3
dredd85 <piotr.siergiej@gmail.com>
2023-08-11 11:40:52 +0200

    Minor print additions

--- 4.Add_Daily_Values.py
+++ 4.Add_Daily_Values.py
@@ -5,13 +5,17 @@ from datetime import date, timedelta
 
 # Function to fetch stock data for a given ticker and return a DataFrame
 def get_stock_data(ticker, start_date, end_date):
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    df = pd.DataFrame(stock_data)
-    df.reset_index(inplace=True)
-    df.drop('Adj Close', axis=1, inplace=True)
-    # Add the 'Ticker' column to the DataFrame
-    df['Ticker'] = ticker
-    return df
+    try:
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
+        df = pd.DataFrame(stock_data)
+        df.reset_index(inplace=True)
+        df.drop('Adj Close', axis=1, inplace=True)
+        # Add the 'Ticker' column to the DataFrame
+        df['Ticker'] = ticker
+        return df
+    except Exception as e:
+        print(f"Failed to fetch data for {ticker}: {e}")
+        return None
 
 df = pd.read_csv('tickers.csv')
 
@@ -32,9 +36,11 @@ for i in range(0, len(unpacked_tickers), batch_size):
 
     for ticker in batch_tickers:
         stock_df = get_stock_data(ticker, start_date, end_date)
+        print(f'{ticker} data fetched')
         batch_data.append(stock_df)
     
     combined_batch = pd.concat(batch_data, ignore_index=True)
+    # Checking if the data is already uploaded
     existing_dates = pd.read_sql(f"SELECT DISTINCT Date FROM Prices WHERE ticker = '{ticker}'", conn)['Date']
     combined_batch = combined_batch[~combined_batch['Date'].isin(existing_dates)]
     combined_batch.to_sql('Prices', conn, if_exists='append', index=False)
