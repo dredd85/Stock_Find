5eee7e665db3a5559cf7dd464ff8625152e7dc3d
dredd85 <piotr.siergiej@gmail.com>
2023-11-13 19:11:26 +0100

    Added additional check if tables are created

--- 1.Create_DB.py
+++ 1.Create_DB.py
@@ -9,6 +9,14 @@ ticker VARCHAR(10) PRIMARY KEY UNIQUE,
 company_name VARCHAR(50),
 industry VARCHAR(50)
 )''')
+table_name = "Stocks" 
+cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+result = cursor.fetchall()
+if result:
+    print(f"Table '{table_name}' created")
+else:
+    print(f"Table '{table_name}' does not exist")
+
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Prices (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,6 +29,14 @@ Close REAL,
 Volume INTEGER,
 FOREIGN KEY (ticker) REFERENCES stocks (ticker)
 )''')
+table_name = "Prices" 
+cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+result = cursor.fetchall()
+if result:
+    print(f"Table '{table_name}' created")
+    print('Table creation complete, continue with Ticker_Load')
+else:
+    print(f"Table '{table_name}' does not exist")
 
 conn.commit()
 conn.close()
