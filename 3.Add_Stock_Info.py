7c08a1bc43f0f76186f14f7bea874430f14870ed
dredd85 <piotr.siergiej@gmail.com>
2023-12-21 21:29:51 +0100

    Added a fix to continue after a 404 error from yfinance

--- 3.Add_Stock_Info.py
+++ 3.Add_Stock_Info.py
@@ -17,20 +17,22 @@ count = 0
 try:
     for i in range(0, len(unpacked_tickers), batch_size):
         batch_tickers = unpacked_tickers[i : i + batch_size]
-        
-        for ticker in batch_tickers:
-            stock = yf.Ticker(ticker)
-            stock_info = stock.info
-            stock_name = stock_info.get('longName', 'N/A')
-            stock_sector = stock_info.get('sector', 'N/A')
-            print(f'Stock Symbol: {ticker}')
-            print(f"Stock Name: {stock_name}")
-            print(f"Sector: {stock_sector}")
-            cursor.execute('''
-            INSERT OR IGNORE INTO Stocks (ticker, company_name, industry) 
-            VALUES (?,?,?)''', (ticker, stock_name, stock_sector))
-            count += 1
-
+        try:
+            for ticker in batch_tickers:
+                stock = yf.Ticker(ticker)
+                stock_info = stock.info
+                stock_name = stock_info.get('longName', 'N/A')
+                stock_sector = stock_info.get('sector', 'N/A')
+                print(f'Stock Symbol: {ticker}')
+                print(f"Stock Name: {stock_name}")
+                print(f"Sector: {stock_sector}")
+                cursor.execute('''
+                INSERT OR IGNORE INTO Stocks (ticker, company_name, industry) 
+                VALUES (?,?,?)''', (ticker, stock_name, stock_sector))
+                count += 1
+        except Exception as e:
+                # Ignore the 404 and continue with the next ticker
+                print(f'Error occurred for ticker {ticker}: {e}')
         conn.commit()
         print(f'Batch {i//batch_size + 1} uploaded to the database')
 
