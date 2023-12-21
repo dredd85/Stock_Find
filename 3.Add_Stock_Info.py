64ba87ecd6a6729b9e10c8c7af822f527611470a
dredd85 <piotr.siergiej@gmail.com>
2023-12-21 21:11:50 +0100

    Revert "Added a fix for 404 yfinance problem"
    This reverts commit 15e395a0b0c842de6c3e68c79ed4dd5bafa83bbf.

--- 3.Add_Stock_Info.py
+++ 3.Add_Stock_Info.py
@@ -7,30 +7,29 @@ tickers = df.values.tolist()
 unpacked_tickers = [row for rows in tickers for row in rows]
 
 count = 0
-batch_size = 50
+
 conn = sqlite3.connect('Stocks.db')
 cursor = conn.cursor()
+
+batch_size = 50
+count = 0
 
 try:
     for i in range(0, len(unpacked_tickers), batch_size):
         batch_tickers = unpacked_tickers[i : i + batch_size]
-
+        
         for ticker in batch_tickers:
-            try:
-                stock = yf.Ticker(ticker)
-                stock_info = stock.info
-                stock_name = stock_info.get('longName', 'N/A')
-                stock_sector = stock_info.get('sector', 'N/A')
-                print(f'Stock Symbol: {ticker}')
-                print(f"Stock Name: {stock_name}")
-                print(f"Sector: {stock_sector}")
-                cursor.execute('''
-                    INSERT OR IGNORE INTO Stocks (ticker, company_name, industry) 
-                    VALUES (?,?,?)''', (ticker, stock_name, stock_sector))
-                count += 1
-            except Exception as e:
-                # Ignore the 404 errors and continue with the next ticker
-                print(f'Error occurred for ticker {ticker}: {e}')
+            stock = yf.Ticker(ticker)
+            stock_info = stock.info
+            stock_name = stock_info.get('longName', 'N/A')
+            stock_sector = stock_info.get('sector', 'N/A')
+            print(f'Stock Symbol: {ticker}')
+            print(f"Stock Name: {stock_name}")
+            print(f"Sector: {stock_sector}")
+            cursor.execute('''
+            INSERT OR IGNORE INTO Stocks (ticker, company_name, industry) 
+            VALUES (?,?,?)''', (ticker, stock_name, stock_sector))
+            count += 1
 
         conn.commit()
         print(f'Batch {i//batch_size + 1} uploaded to the database')
@@ -43,3 +42,4 @@ finally:
     conn.close()
 
 print(f'Uploaded {count} ticker info to the database')
+
